a39ce07ad505a2d8044ff5c8350fb772b136fc71
Dannitygraphix <dellwingz@gmail.com>
2023-03-23 09:25:14 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,13 @@
 #include "function_pointers.h"
 
 /**
- *  * print_name - prints a name
- *   * @name: name to print
- *    * @f: pointer to the printing function
- *    */
-void print_name(char *name, void (*f)(char *))
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
+void print_name(char *name, void (*f) (char *))
 {
-		if (!name || !f)
-					return;
-			f(name);
+	if (f == NULL)
+		return;
+	f(name);
 }
